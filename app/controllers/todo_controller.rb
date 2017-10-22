680c80e5a43ff006a325b0692b2e07d98dea1f36
roberto <roberto.rodriguez@epicacademy.org>
2017-10-22 17:58:52 +0000

    add new commit and create actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -6,4 +6,15 @@ def index
       
 
 end
+def new
+    
+end
+def create
+
+  t = Todo.new
+  t.description = params['description']
+  t.pomodoro_estimate = params['pomodoro_estimate']
+  t.save
+  redirect_to "/todo/show/#{ t.id }"
+end
 end 
