a47d7da538977cf98a17faeb7ab1cdb77324c0cd
roberto <roberto.rodriguez@epicacademy.org>
2017-10-22 14:50:28 +0000

    postgres install

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,31 @@ class TodoController < ApplicationController
 def index 
     end
     def show
+        todo_id = params[:id]
+        if todo_id=='1'
         @todo_description ="make the curriculum"
         @todo_pomodoro_estimate = 4
-end
+        
+ elsif todo_id=='2'
+        @todo_description ="A"
+        @todo_pomodoro_estimate = 4
+    end
+        if todo_id=='3'
+        @todo_description ="B"
+        @todo_pomodoro_estimate = 4
+    end
+        if todo_id=='4'
+        @todo_description ="C"
+        @todo_pomodoro_estimate = 4
+    end
+        if todo_id=='5'
+        @todo_description ="D"
+        @todo_pomodoro_estimate = 4
+       end
+        if todo_id=='6'
+        @todo_description ="make the curriculum"
+        @todo_pomodoro_estimate = 4
+    end
 
 end
+end 
